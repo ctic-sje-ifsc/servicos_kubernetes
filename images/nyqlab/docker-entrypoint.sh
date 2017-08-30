ffc777119e329ee0400a6457d4565f4700fe69e1
souzagaabriel <souzagaabriel@gmail.com>
2017-08-30 20:15:12 -0300

    Adicionado x2goclient no Nyqlab e melhoria na configuração do ssh.

--- images/nyqlab/docker-entrypoint.sh
+++ images/nyqlab/docker-entrypoint.sh
@@ -2,4 +2,7 @@
 
 set -e
 
+# SSH login fix. Otherwise user is kicked off after login
+sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
+
 exec "$@"
