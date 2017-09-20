5c18c95fa155e298a88ba6e86efadba0951489b7
Ederson Torresini <boidacarapreta@air.local>
2017-09-20 10:36:39 -0300

    Imagem nylab movida para ctic-sje-ifsc/imagens.

--- images/nyqlab/docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-# SSH login fix. Otherwise user is kicked off after login
-sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
-
-exec "$@"
